84c68dffb66d978b9539e7ca63171650cbc5b523
yusukezzz <g0632198@gmail.com>
2016-10-17 20:24:32 +0900

    use singleton object

--- app/src/main/kotlin/net/yusukezzz/ssmtc/services/SearchQueryBuilder.kt
+++ app/src/main/kotlin/net/yusukezzz/ssmtc/services/SearchQueryBuilder.kt
@@ -2,24 +2,22 @@ package net.yusukezzz.ssmtc.services
 
 import org.apache.commons.codec.net.URLCodec
 
-class SearchQueryBuilder {
-    companion object {
-        val urlencoder = URLCodec("UTF-8")
+object SearchQueryBuilder {
+    val urlencoder = URLCodec("UTF-8")
 
-        fun build(params: TimelineParameter): TimelineParameter {
-            val queries = mutableListOf(params.query)
+    fun build(params: TimelineParameter): TimelineParameter {
+        val queries = mutableListOf(params.query)
 
-            if (!params.includeRetweets) {
-                queries.add("exclude:retweets")
-            }
-
-            when (params.filter.showing) {
-                TimelineFilter.Showing.ANY_MEDIA -> queries.add("filter:media")
-                TimelineFilter.Showing.PHOTO -> queries.add("filter:images")
-                TimelineFilter.Showing.VIDEO -> queries.add("filter:videos")
-            }
-
-            return params.copy(query = urlencoder.encode(queries.joinToString(" ")))
+        if (!params.includeRetweets) {
+            queries.add("exclude:retweets")
         }
+
+        when (params.filter.showing) {
+            TimelineFilter.Showing.ANY_MEDIA -> queries.add("filter:media")
+            TimelineFilter.Showing.PHOTO -> queries.add("filter:images")
+            TimelineFilter.Showing.VIDEO -> queries.add("filter:videos")
+        }
+
+        return params.copy(query = urlencoder.encode(queries.joinToString(" ")))
     }
 }
--- app/src/main/kotlin/net/yusukezzz/ssmtc/util/TextUtil.kt
+++ app/src/main/kotlin/net/yusukezzz/ssmtc/util/TextUtil.kt
@@ -10,85 +10,83 @@ import net.yusukezzz.ssmtc.data.json.Tweet
 import net.yusukezzz.ssmtc.screens.timeline.TimelineAdapter.TimelineEventListener
 import org.apache.commons.lang3.StringEscapeUtils
 
-class TextUtil {
-    companion object {
-        val SCREEN_NAME_PATTERN = Regex("@[a-zA-Z0-9_]+")
-        val HASH_TAG_PATTERN = Regex("#\\w+")
+object TextUtil {
+    val SCREEN_NAME_PATTERN = Regex("@[a-zA-Z0-9_]+")
+    val HASH_TAG_PATTERN = Regex("#\\w+")
 
-        fun milliSecToTime(millis: Int): String {
-            val sec = millis / 1000
-            val m = sec % 3600 / 60
-            val s = sec % 3600 % 60
+    fun milliSecToTime(millis: Int): String {
+        val sec = millis / 1000
+        val m = sec % 3600 / 60
+        val s = sec % 3600 % 60
 
-            return "%01d:%02d".format(m, s)
-        }
+        return "%01d:%02d".format(m, s)
+    }
 
-        fun formattedText(tweet: Tweet, listener: TimelineEventListener, removeQuote: Boolean = false): CharSequence {
-            val entities = tweet.entities
-            val decodedText = StringEscapeUtils.unescapeHtml4(tweet.text)
-            if (null == entities.urls) return decodedText
+    fun formattedText(tweet: Tweet, listener: TimelineEventListener, removeQuote: Boolean = false): CharSequence {
+        val entities = tweet.entities
+        val decodedText = StringEscapeUtils.unescapeHtml4(tweet.text)
+        if (null == entities.urls) return decodedText
 
-            val urls = entities.urls.map(::FormattedUrl)
-            val medias = entities.media?.map(::FormattedMedia) ?: listOf()
-            val combined = (urls + medias).sortedBy { it.start }
+        val urls = entities.urls.map(::FormattedUrl)
+        val medias = entities.media?.map(::FormattedMedia) ?: listOf()
+        val combined = (urls + medias).sortedBy { it.start }
 
-            val lastUrl = if (removeQuote) {
-                urls
-            } else {
-                medias
-            }.lastOrNull()?.shortUrl ?: ""
-            val spannable = SpannableStringBuilder(decodedText.replace(lastUrl, ""))
+        val lastUrl = if (removeQuote) {
+            urls
+        } else {
+            medias
+        }.lastOrNull()?.shortUrl ?: ""
+        val spannable = SpannableStringBuilder(decodedText.replace(lastUrl, ""))
 
-            replaceUrlEntities(spannable, combined, listener)
-            replaceScreenName(spannable, listener)
-            replaceHashTag(spannable, listener)
+        replaceUrlEntities(spannable, combined, listener)
+        replaceScreenName(spannable, listener)
+        replaceHashTag(spannable, listener)
 
-            return spannable
-        }
+        return spannable
+    }
 
-        private fun replaceUrlEntities(spannable: SpannableStringBuilder,
-                                       entities: List<FormattedUrl>,
-                                       listener: TimelineEventListener) {
-            entities.forEachIndexed { i, entity ->
-                val start = spannable.indexOf(entity.shortUrl)
-                val end = start + entity.shortUrl.length
-
-                if (start >= 0) {
-                    spannable.replace(start, end, entity.displayUrl)
-                    val displayEnd = start + entity.displayUrl.length
-                    val span = object: ClickableSpan() {
-                        override fun onClick(widget: View?) {
-                            listener.onUrlClick(entity.url)
-                        }
-                    }
-
-                    spannable.setSpan(span, start, displayEnd, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
-                }
-            }
-        }
-
-        private fun replaceScreenName(spannable: SpannableStringBuilder,
-                                      listener: TimelineEventListener) {
-            SCREEN_NAME_PATTERN.findAll(spannable).forEach {
-                val span = object: ClickableSpan() {
-                    override fun onClick(widget: View?) {
-                        listener.onScreenNameClick(it.value.removePrefix("@"))
-                    }
-                }
-                spannable.setSpan(span, it.range.first, it.range.last + 1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
-            }
-        }
-
-        private fun replaceHashTag(spannable: SpannableStringBuilder,
+    private fun replaceUrlEntities(spannable: SpannableStringBuilder,
+                                   entities: List<FormattedUrl>,
                                    listener: TimelineEventListener) {
-            HASH_TAG_PATTERN.findAll(spannable).forEach {
-                val span = object: ClickableSpan() {
+        entities.forEachIndexed { i, entity ->
+            val start = spannable.indexOf(entity.shortUrl)
+            val end = start + entity.shortUrl.length
+
+            if (start >= 0) {
+                spannable.replace(start, end, entity.displayUrl)
+                val displayEnd = start + entity.displayUrl.length
+                val span = object : ClickableSpan() {
                     override fun onClick(widget: View?) {
-                        listener.onHashTagClick(it.value)
+                        listener.onUrlClick(entity.url)
                     }
                 }
-                spannable.setSpan(span, it.range.first, it.range.last + 1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+                spannable.setSpan(span, start, displayEnd, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
             }
+        }
+    }
+
+    private fun replaceScreenName(spannable: SpannableStringBuilder,
+                                  listener: TimelineEventListener) {
+        SCREEN_NAME_PATTERN.findAll(spannable).forEach {
+            val span = object : ClickableSpan() {
+                override fun onClick(widget: View?) {
+                    listener.onScreenNameClick(it.value.removePrefix("@"))
+                }
+            }
+            spannable.setSpan(span, it.range.first, it.range.last + 1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        }
+    }
+
+    private fun replaceHashTag(spannable: SpannableStringBuilder,
+                               listener: TimelineEventListener) {
+        HASH_TAG_PATTERN.findAll(spannable).forEach {
+            val span = object : ClickableSpan() {
+                override fun onClick(widget: View?) {
+                    listener.onHashTagClick(it.value)
+                }
+            }
+            spannable.setSpan(span, it.range.first, it.range.last + 1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
         }
     }
 }
