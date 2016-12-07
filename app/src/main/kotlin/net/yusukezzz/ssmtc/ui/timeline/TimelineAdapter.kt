bee2e96d09fcf66aa1e64646d3a026a12ffe8a71
yusukezzz <g0632198@gmail.com>
2016-12-07 12:47:35 +0900

    static ViewHolder classes

--- app/src/main/kotlin/net/yusukezzz/ssmtc/ui/timeline/TimelineAdapter.kt
+++ app/src/main/kotlin/net/yusukezzz/ssmtc/ui/timeline/TimelineAdapter.kt
@@ -23,6 +23,7 @@ import java.text.DecimalFormat
 
 class TimelineAdapter(val listener: TimelineEventListener): RecyclerView.Adapter<ViewHolder>() {
     companion object {
+        const val LARGE_IMAGE_TAG = "large_image_tag"
         const val VIEW_TYPE_NO_MEDIA = 0
         const val VIEW_TYPE_HAS_PHOTO_1 = 1
         const val VIEW_TYPE_HAS_PHOTO_2 = 2
@@ -35,6 +36,132 @@ class TimelineAdapter(val listener: TimelineEventListener): RecyclerView.Adapter
             VIEW_TYPE_HAS_PHOTO_3,
             VIEW_TYPE_HAS_PHOTO_4
         )
+
+        private open class TweetViewHolder(view: View,
+                                           val listener: TimelineEventListener) : ViewHolder(view) {
+            private val numberFormatter = DecimalFormat("#,###,###")
+
+            open fun bindTweet(tweet: Tweet) {
+                if (tweet.isRetweet) {
+                    handleRetweeted(tweet)
+                } else if (tweet.isRetweetWithQuoted) {
+                    handleQuoted(tweet)
+                } else {
+                    handleTweet(tweet)
+                }
+            }
+
+            private fun handleTweet(tweet: Tweet, removeQuote: Boolean = false) {
+                itemView.tweet_retweeted_container.visibility = View.GONE
+                itemView.quote_container.visibility = View.GONE
+                val formatted = TextUtil.formattedText(tweet, listener, removeQuote).trim()
+                with(tweet) {
+                    val profImg = itemView.tweet_user_image
+                    Picasso.with(itemView.context)
+                        .load(user.profileImageUrl)
+                        .fit().centerCrop()
+                        .transform(RoundedTransformation(8))
+                        .into(profImg)
+                    itemView.tweet_user_name.text = user.name
+                    itemView.tweet_user_screen_name.text = "@" + user.screenName
+                    itemView.tweet_date.text = DateUtils.getRelativeTimeSpanString(created_at.toEpochSecond() * 1000L)
+                    itemView.tweet_text.text = formatted
+                    itemView.tweet_text.movementMethod = LinkMovementMethod.getInstance()
+                    itemView.tweet_text.visibility = if (formatted.isEmpty()) View.GONE else View.VISIBLE
+                }
+                handleReaction(tweet)
+            }
+
+            private fun handleReaction(tweet: Tweet) {
+                itemView.tweet_retweet_count.text = ""
+                itemView.tweet_like_count.text = ""
+
+                itemView.ic_twitter_reply.setOnClickListener { listener.onReplyClick(tweet) }
+
+                if (0 < tweet.retweet_count) {
+                    itemView.tweet_retweet_count.text = numberFormatter.format(tweet.retweet_count)
+                }
+                val retweetColor = if (tweet.retweeted) {
+                    R.color.action_retweet_on
+                } else {
+                    R.color.action_icon_default
+                }
+                itemView.ic_twitter_retweet.setColorFilter(ContextCompat.getColor(itemView.context, retweetColor))
+                itemView.ic_twitter_retweet.setOnClickListener { listener.onRetweetClick(tweet) }
+
+                if (0 < tweet.favorite_count) {
+                    itemView.tweet_like_count.text = numberFormatter.format(tweet.favorite_count)
+                }
+                val likeColor = if (tweet.favorited) {
+                    R.color.action_like_on
+                } else {
+                    R.color.action_icon_default
+                }
+                itemView.ic_twitter_like.setColorFilter(ContextCompat.getColor(itemView.context, likeColor))
+                itemView.ic_twitter_like.setOnClickListener { listener.onLikeClick(tweet) }
+            }
+
+            private fun handleRetweeted(tweet: Tweet) {
+                handleTweet(tweet.retweeted_status!!)
+                itemView.tweet_retweeted_message.text = tweet.user.name + itemView.resources.getString(R.string.retweeted_by)
+                itemView.tweet_retweeted_container.visibility = View.VISIBLE
+            }
+
+            private fun handleQuoted(tweet: Tweet) {
+                handleTweet(tweet, true)
+                val quoted = tweet.quoted_status!!
+                itemView.quote_text.text = TextUtil.formattedText(quoted, listener)
+                itemView.quote_text.movementMethod = LinkMovementMethod.getInstance()
+                itemView.quote_user_name.text = quoted.user.name
+                itemView.quote_user_screen_name.text = "@" + quoted.user.screenName
+                itemView.quote_container.visibility = View.VISIBLE
+            }
+        }
+
+        private class TweetWithPhotoViewHolder(view: View,
+                                               listener: TimelineEventListener) : TweetViewHolder(view, listener) {
+            private val mediaViewIdLists: List<List<Int>> = listOf(
+                listOf(R.id.media_photo_single),
+                listOf(R.id.media_photo_two_1, R.id.media_photo_two_2),
+                listOf(R.id.media_photo_three_1, R.id.media_photo_three_2, R.id.media_photo_three_3),
+                listOf(R.id.media_photo_four_1, R.id.media_photo_four_2, R.id.media_photo_four_3, R.id.media_photo_four_4)
+            )
+
+            override fun bindTweet(tweet: Tweet) {
+                super.bindTweet(tweet)
+                handlePhoto(tweet.allMedia)
+            }
+
+            fun handlePhoto(photos: List<Media>) {
+                val num = photos.size
+                val gallery_photos = photos.map { it.large_url }
+                photos.forEachIndexed { i, m ->
+                    val ids = mediaViewIdLists[num - 1]
+                    val imgView = itemView.findViewById(ids[i]) as ImageView
+                    imgView.setOnClickListener { listener.onImageClick(gallery_photos, i) }
+                    Picasso.with(itemView.context).load(m.small_url).fit().centerCrop().tag(LARGE_IMAGE_TAG).into(imgView)
+                }
+            }
+        }
+
+        private class TweetWithVideoViewHolder(view: View,
+                                               listener: TimelineEventListener) : TweetViewHolder(view, listener) {
+
+            override fun bindTweet(tweet: Tweet) {
+                super.bindTweet(tweet)
+                handleVideo(tweet.allMedia.first())
+            }
+
+            fun handleVideo(video: Media) {
+                if (null == video.video_info) return
+
+                println(video.video_info)
+                itemView.media_video_time.text = TextUtil.milliSecToTime(video.video_info.duration_millis)
+                val imgView = itemView.media_video_thumbnail
+                imgView.setOnClickListener { listener.onVideoClick(video.video_info) }
+                Picasso.with(itemView.context).load(video.small_url).fit().centerCrop().tag(LARGE_IMAGE_TAG).into(imgView)
+            }
+        }
     }
 
     interface TimelineEventListener {
@@ -110,135 +237,5 @@ class TimelineAdapter(val listener: TimelineEventListener): RecyclerView.Adapter
     fun add(tweets: List<Tweet>) {
         timeline.addAll(tweets)
         notifyDataSetChanged()
-    }
-
-    open class TweetViewHolder(val view: View,
-                               val listener: TimelineEventListener): ViewHolder(view) {
-        companion object {
-            val LARGE_IMAGE_TAG = "large_image_tag"
-        }
-
-        private val numberFormatter = DecimalFormat("#,###,###")
-
-        open fun bindTweet(tweet: Tweet) {
-            if (tweet.isRetweet) {
-                handleRetweeted(tweet)
-            } else if (tweet.isRetweetWithQuoted) {
-                handleQuoted(tweet)
-            } else {
-                handleTweet(tweet)
-            }
-        }
-
-        private fun handleTweet(tweet: Tweet, removeQuote: Boolean = false) {
-            itemView.tweet_retweeted_container.visibility = View.GONE
-            itemView.quote_container.visibility = View.GONE
-            val formatted = TextUtil.formattedText(tweet, listener, removeQuote).trim()
-            with(tweet) {
-                val profImg = itemView.tweet_user_image
-                Picasso.with(itemView.context)
-                    .load(user.profileImageUrl)
-                    .fit().centerCrop()
-                    .transform(RoundedTransformation(8))
-                    .into(profImg)
-                itemView.tweet_user_name.text = user.name
-                itemView.tweet_user_screen_name.text = "@" + user.screenName
-                itemView.tweet_date.text = DateUtils.getRelativeTimeSpanString(created_at.toEpochSecond() * 1000L)
-                itemView.tweet_text.text = formatted
-                itemView.tweet_text.movementMethod = LinkMovementMethod.getInstance()
-                itemView.tweet_text.visibility = if (formatted.isEmpty()) View.GONE else View.VISIBLE
-            }
-            handleReaction(tweet)
-        }
-
-        private fun handleReaction(tweet: Tweet) {
-            itemView.tweet_retweet_count.text = ""
-            itemView.tweet_like_count.text = ""
-
-            itemView.ic_twitter_reply.setOnClickListener { listener.onReplyClick(tweet) }
-
-            if (0 < tweet.retweet_count) {
-                itemView.tweet_retweet_count.text = numberFormatter.format(tweet.retweet_count)
-            }
-            val retweetColor = if (tweet.retweeted) {
-                R.color.action_retweet_on
-            } else {
-                R.color.action_icon_default
-            }
-            itemView.ic_twitter_retweet.setColorFilter(ContextCompat.getColor(itemView.context, retweetColor))
-            itemView.ic_twitter_retweet.setOnClickListener { listener.onRetweetClick(tweet) }
-
-            if (0 < tweet.favorite_count) {
-                itemView.tweet_like_count.text = numberFormatter.format(tweet.favorite_count)
-            }
-            val likeColor = if (tweet.favorited) {
-                R.color.action_like_on
-            } else {
-                R.color.action_icon_default
-            }
-            itemView.ic_twitter_like.setColorFilter(ContextCompat.getColor(itemView.context, likeColor))
-            itemView.ic_twitter_like.setOnClickListener { listener.onLikeClick(tweet) }
-        }
-
-        private fun handleRetweeted(tweet: Tweet) {
-            handleTweet(tweet.retweeted_status!!)
-            itemView.tweet_retweeted_message.text = tweet.user.name + itemView.resources.getString(R.string.retweeted_by)
-            itemView.tweet_retweeted_container.visibility = View.VISIBLE
-        }
-
-        private fun handleQuoted(tweet: Tweet) {
-            handleTweet(tweet, true)
-            val quoted = tweet.quoted_status!!
-            itemView.quote_text.text = TextUtil.formattedText(quoted, listener)
-            itemView.quote_text.movementMethod = LinkMovementMethod.getInstance()
-            itemView.quote_user_name.text = quoted.user.name
-            itemView.quote_user_screen_name.text = "@" + quoted.user.screenName
-            itemView.quote_container.visibility = View.VISIBLE
-        }
-    }
-
-    class TweetWithPhotoViewHolder(view: View,
-                                   listener: TimelineEventListener): TweetViewHolder(view, listener) {
-        private val mediaViewIdLists: List<List<Int>> = listOf(
-            listOf(R.id.media_photo_single),
-            listOf(R.id.media_photo_two_1, R.id.media_photo_two_2),
-            listOf(R.id.media_photo_three_1, R.id.media_photo_three_2, R.id.media_photo_three_3),
-            listOf(R.id.media_photo_four_1, R.id.media_photo_four_2, R.id.media_photo_four_3, R.id.media_photo_four_4)
-        )
-
-        override fun bindTweet(tweet: Tweet) {
-            super.bindTweet(tweet)
-            handlePhoto(tweet.allMedia)
-        }
-
-        fun handlePhoto(photos: List<Media>) {
-            val num = photos.size
-            val gallery_photos = photos.map { it.large_url }
-            photos.forEachIndexed { i, m ->
-                val ids = mediaViewIdLists[num - 1]
-                val imgView = itemView.findViewById(ids[i]) as ImageView
-                imgView.setOnClickListener { listener.onImageClick(gallery_photos, i) }
-                Picasso.with(itemView.context).load(m.small_url).fit().centerCrop().tag(LARGE_IMAGE_TAG).into(imgView)
-            }
-        }
-    }
-
-    class TweetWithVideoViewHolder(view: View,
-                                   listener: TimelineEventListener): TweetViewHolder(view, listener) {
-
-        override fun bindTweet(tweet: Tweet) {
-            super.bindTweet(tweet)
-            handleVideo(tweet.allMedia.first())
-        }
-
-        fun handleVideo(video: Media) {
-            if (null == video.video_info) return
-
-            println(video.video_info)
-            itemView.media_video_time.text = TextUtil.milliSecToTime(video.video_info.duration_millis)
-            val imgView = itemView.media_video_thumbnail
-            imgView.setOnClickListener { listener.onVideoClick(video.video_info) }
-            Picasso.with(itemView.context).load(video.small_url).fit().centerCrop().tag(LARGE_IMAGE_TAG).into(imgView)
-        }
     }
 }
