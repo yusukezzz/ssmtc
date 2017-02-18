f2f99ff2f201504e303ff42f80964468ebcfaee5
yusukezzz <g0632198@gmail.com>
2017-02-19 08:33:55 +0900

    fix og parser

--- app/src/main/kotlin/net/yusukezzz/ssmtc/data/og/OpenGraphParser.kt
+++ app/src/main/kotlin/net/yusukezzz/ssmtc/data/og/OpenGraphParser.kt
@@ -6,14 +6,14 @@ import java.nio.charset.Charset
 import java.nio.charset.StandardCharsets
 
 object OpenGraphParser {
-    private val OG_TITLE = Regex("(og|twitter):title")
-    private val OG_IMAGE = Regex("(\"(og|twitter):image\"|'(og|twitter):image')")
-    private val OG_URL = Regex("(og|twitter):url")
+    private val OG_TITLE = Regex("og:title")
+    private val OG_IMAGE = Regex("(\"og:image\"|'og:image')")
+    private val OG_URL = Regex("og:url")
 
     private val REGEX_OPTS = setOf(RegexOption.IGNORE_CASE, RegexOption.MULTILINE)
     private val META_TAG = Regex("<meta.+?>", REGEX_OPTS)
-    private val META_CHARSET_TAG = Regex("<meta.+?charset=(.+?)(\\s|>)", REGEX_OPTS)
-    private val TITLE_TAG = Regex("<title>(.+?)</title>", REGEX_OPTS)
+    private val META_CHARSET_TAG = Regex("<meta.+?charset=(.+?)(\\s|/?>)", REGEX_OPTS)
+    private val TITLE_TAG = Regex("<title.*?>(.+?)</title>", REGEX_OPTS)
     private val HEAD_END_TAG = Regex("</head>", REGEX_OPTS)
     private const val CONTENT_KEY = "content="
 
