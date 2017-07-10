a376005e00135c238d4c1c4aaf1cc60d09b1cb20
yusukezzz <g0632198@gmail.com>
2017-07-10 14:24:52 +0900

    add test

--- app/src/test/kotlin/net/yusukezzz/ssmtc/data/og/OpenGraphParserTest.kt
+++ app/src/test/kotlin/net/yusukezzz/ssmtc/data/og/OpenGraphParserTest.kt
@@ -8,11 +8,21 @@ import org.junit.runners.JUnit4
 @RunWith(JUnit4::class)
 class OpenGraphParserTest {
     @Test
-    fun testParseSjis() {
+    fun parseEncoded() {
+        val reader = this.javaClass.classLoader.getResourceAsStream("html/utf8.html").bufferedReader(Charsets.UTF_8)
+        val og = OpenGraphParser.parse("test", reader)
+
+        assertEquals("OGタイトル UTF-8", og.title)
+        assertEquals("http://example.com", og.url)
+        assertEquals("http://image.com", og.image)
+    }
+
+    @Test
+    fun parseBytes() {
         val input = this.javaClass.classLoader.getResourceAsStream("html/sjis.html")
         val og = OpenGraphParser.parse("test", input)
 
-        assertEquals("OGタイトル", og.title)
+        assertEquals("OGタイトル SJIS", og.title)
         assertEquals("http://example.com", og.url)
         assertEquals("http://image.com", og.image)
     }
